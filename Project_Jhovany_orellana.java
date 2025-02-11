f246dd6fb36151846d0f628530a6a680b9d2caa2
jhovanyorellana <jhovayismart@gmail.com>
2025-02-11 17:41:15 -0500

    Create Project_Jhovany_orellana.java

---
+++ Project_Jhovany_orellana.java
@@ -0,0 +1,6 @@
+public class Project_Jhovany_orellana {  // Change to match your name
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("This is my CSC251 project.");
+    }
+}
